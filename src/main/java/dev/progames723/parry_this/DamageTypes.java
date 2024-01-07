b41147af7e49f9770c09590ba8fe73aee4f9ac12
Vovan723 <93779034+Vovan723@users.noreply.github.com>
2024-01-07 18:31:06 +0700

    this is broken rn please don't compile

--- src/main/java/dev/progames723/parry_this/DamageTypes.java
+++ src/main/java/dev/progames723/parry_this/DamageTypes.java
@@ -2,17 +2,107 @@ package dev.progames723.parry_this;
 
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.DamageTypeTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Vector;
+
 public class DamageTypes {
-	public static final RegistryKey<DamageType> DAMAGE_TYPE_PENETRATION = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, new Identifier(FabricatedParry.MOD_ID, "penetration"));
+	public static final RegistryKey<DamageType> weDontTalkAboutThisOne = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, new Identifier(FabricatedParry.MOD_ID, "penetration"));
 
 	@NotNull
 	public static DamageSource of(@NotNull World world, RegistryKey<DamageType> key) {
+		return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
+	}
+	@NotNull
+	public static DamageSource of(@NotNull PlayerEntity entity, RegistryKey<DamageType> key) {
+		return new DamageSource(entity.getWorld().getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
+	}
+	@NotNull
+	public static DamageSource of(@NotNull MinecraftServer server, RegistryKey<DamageType> key) {
+		return new DamageSource(server.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
+	}
+	@NotNull
+	public static DamageSource of(@NotNull WorldView worldView, RegistryKey<DamageType> key) {
+		return new DamageSource(worldView.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
+	}
+	@NotNull
+	public static DamageSource applyArmorIgnoringDamageTags(@NotNull PlayerEntity entity, RegistryKey<DamageType> key){
+		World world = entity.getWorld();
+		TagKey<DamageType> damageTypeTag1 = DamageTypeTags.BYPASSES_ARMOR;
+		TagKey<DamageType> damageTypeTag2 = DamageTypeTags.BYPASSES_COOLDOWN;
+		TagKey<DamageType> damageTypeTag3 = DamageTypeTags.BYPASSES_EFFECTS;
+		TagKey<DamageType> damageTypeTag4 = DamageTypeTags.BYPASSES_ENCHANTMENTS;
+		TagKey<DamageType> damageTypeTag5 = DamageTypeTags.BYPASSES_SHIELD;
+		TagKey<DamageType> damageTypeTag6 = DamageTypeTags.BYPASSES_INVULNERABILITY;
+		TagKey<DamageType> damageTypeTag7 = DamageTypeTags.BYPASSES_RESISTANCE;
+		Vector<TagKey<DamageType>> vector = new Vector<>(6);
+		vector.add(damageTypeTag1);
+		vector.add(damageTypeTag2);
+		vector.add(damageTypeTag3);
+		vector.add(damageTypeTag4);
+		vector.add(damageTypeTag5);
+		vector.add(damageTypeTag6);
+		vector.add(damageTypeTag7);
+		Enumeration<TagKey<DamageType>> enumeration = vector.elements();
+		Collection<TagKey<DamageType>> damageTypeTags = Collections.list(enumeration);
+		world.getDamageSources().registry.entryOf(key).setTags(damageTypeTags);
+		return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
+	}
+	@NotNull
+	public static DamageSource applyArmorIgnoringDamageTags(@NotNull World world, RegistryKey<DamageType> key){
+		TagKey<DamageType> damageTypeTag1 = DamageTypeTags.BYPASSES_ARMOR;
+		TagKey<DamageType> damageTypeTag2 = DamageTypeTags.BYPASSES_COOLDOWN;
+		TagKey<DamageType> damageTypeTag3 = DamageTypeTags.BYPASSES_EFFECTS;
+		TagKey<DamageType> damageTypeTag4 = DamageTypeTags.BYPASSES_ENCHANTMENTS;
+		TagKey<DamageType> damageTypeTag5 = DamageTypeTags.BYPASSES_SHIELD;
+		TagKey<DamageType> damageTypeTag6 = DamageTypeTags.BYPASSES_INVULNERABILITY;
+		TagKey<DamageType> damageTypeTag7 = DamageTypeTags.BYPASSES_RESISTANCE;
+		Vector<TagKey<DamageType>> vector = new Vector<>(6);
+		vector.add(damageTypeTag1);
+		vector.add(damageTypeTag2);
+		vector.add(damageTypeTag3);
+		vector.add(damageTypeTag4);
+		vector.add(damageTypeTag5);
+		vector.add(damageTypeTag6);
+		vector.add(damageTypeTag7);
+		Enumeration<TagKey<DamageType>> enumeration = vector.elements();
+		Collection<TagKey<DamageType>> damageTypeTags = Collections.list(enumeration);
+		world.getDamageSources().registry.entryOf(key).setTags(damageTypeTags);
+		return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
+	}
+	@NotNull
+	public static DamageSource applyArmorIgnoringDamageTags(@NotNull MinecraftServer server, RegistryKey<DamageType> key){
+		World world = server.getOverworld();
+		TagKey<DamageType> damageTypeTag1 = DamageTypeTags.BYPASSES_ARMOR;
+		TagKey<DamageType> damageTypeTag2 = DamageTypeTags.BYPASSES_COOLDOWN;
+		TagKey<DamageType> damageTypeTag3 = DamageTypeTags.BYPASSES_EFFECTS;
+		TagKey<DamageType> damageTypeTag4 = DamageTypeTags.BYPASSES_ENCHANTMENTS;
+		TagKey<DamageType> damageTypeTag5 = DamageTypeTags.BYPASSES_SHIELD;
+		TagKey<DamageType> damageTypeTag6 = DamageTypeTags.BYPASSES_INVULNERABILITY;
+		TagKey<DamageType> damageTypeTag7 = DamageTypeTags.BYPASSES_RESISTANCE;
+		Vector<TagKey<DamageType>> vector = new Vector<>(6);
+		vector.add(damageTypeTag1);
+		vector.add(damageTypeTag2);
+		vector.add(damageTypeTag3);
+		vector.add(damageTypeTag4);
+		vector.add(damageTypeTag5);
+		vector.add(damageTypeTag6);
+		vector.add(damageTypeTag7);
+		Enumeration<TagKey<DamageType>> enumeration = vector.elements();
+		Collection<TagKey<DamageType>> damageTypeTags = Collections.list(enumeration);
+		world.getDamageSources().registry.entryOf(key).setTags(damageTypeTags);
 		return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
 	}
 }
--- src/main/java/dev/progames723/parry_this/FabricatedParry.java
+++ src/main/java/dev/progames723/parry_this/FabricatedParry.java
@@ -1,49 +1,123 @@
 package dev.progames723.parry_this;
 
-import fuzs.puzzleslib.api.entity.v1.DamageSourcesHelper;
 import fuzs.puzzleslib.api.event.v1.core.EventResult;
 import fuzs.puzzleslib.api.event.v1.data.MutableFloat;
 import fuzs.puzzleslib.api.event.v1.entity.living.LivingHurtCallback;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.TypedActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FabricatedParry implements ModInitializer {
 	//Если не запускается то пропиши .\gradlew.bat clean и .\gradlew.bat --refresh-dependencies
+	public static boolean perfectParry = false;
 	public static boolean parry = false;
 	public static boolean lateParry = false;
 	public static double vulnerable = 0.0;
 	public static double resistant = 0.0;
+	public static int waitingTicks = 0;
+	public static int cooldown = 0;
 	public static float eventDamage = 0.0f;
-
+	public static final float maxFloatValue = 340282356779733661637539395458142568447.999999999f; //help me
+	public static final float minFloatValue = -340282356779733661637539395458142568447.999999999f; //help me
+	public static int waitTicks = 0;
+	public static Runnable runnable = null;
 	public static final String MOD_ID = "parry_this";
 	public static final Logger LOGGER = LoggerFactory.getLogger("Fabricated Parry");
 	public static void sendMessage(Object message, PlayerEntity player){
 		player.sendMessage(Text.of(String.valueOf(message)));
 	}
-
+	public static void waitNumberTicks(int ticks, Runnable action){
+		//Usage: waitNumberTicks(anyInt, () -> {});
+		//Using ServerTickEvents.START_WORLD_TICK for ticking
+		if (ticks < 0){
+			ticks = ticks * -1;
+		}
+		waitTicks = ticks;
+		runnable = action;
+	}
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initialized!");
+		PacketByteBuf data = PacketByteBufs.create();
 		ModItems.registerModItems();
 		// НЕ ТРОГАТЬ, КОД РАБОЧИЙ
+		ServerTickEvents.START_WORLD_TICK.register((serverWorld) -> {
+			MinecraftServer server = serverWorld.getServer();
+			if (runnable != null) {
+				if (waitTicks > 0){
+					waitTicks--;
+				} else if (waitTicks == 0) {
+					runnable.run();
+				}
+			}
+			if (cooldown > 20){
+				cooldown = 20;
+			} else if (cooldown < 0) {
+				cooldown = cooldown*-1;
+			}
+			if (cooldown > 16 && cooldown <= 20){
+				perfectParry = true;
+				parry = false;
+				lateParry = false;
+			} else if (cooldown > 8 && cooldown <= 16){
+				perfectParry = false;
+				parry = true;
+				lateParry = false;
+			} else if (cooldown > 0 && cooldown <= 8){
+				perfectParry = false;
+				parry = false;
+				lateParry = true;
+			} else if (cooldown == 0){
+				perfectParry = false;
+				parry = false;
+				lateParry = false;
+			}
+			if (cooldown > 0){
+				cooldown--;
+			}
+		});
+		LivingHurtCallback.EVENT.register((entity, damageSource, damage) -> {
+			if (entity instanceof PlayerEntity){
+				PlayerEntity player = (PlayerEntity) entity;
+				if (player.getInventory().getMainHandStack().isIn(ItemTags.SWORDS) && player.getItemCooldownManager().isCoolingDown(player.getInventory().getMainHandStack().getItem())){
+					if (perfectParry && !parry && !lateParry){
+						Object message = Text.translatableWithFallback("perfect_parry.message", "A perfect parry! You ignored %1$s damage!", damage.getAsFloat());
+						player.sendMessage(Text.of(String.valueOf(message)), true);
+						return EventResult.INTERRUPT;
+					} else if (!perfectParry && parry && !lateParry){
+						eventDamage = damage.getAsFloat();
+						damage = MutableFloat.fromValue((float) Math.pow(eventDamage, 0.05)*0.5f);
+						return EventResult.PASS;
+					} else if (!perfectParry && !parry && lateParry) {
+						eventDamage = damage.getAsFloat();
+						damage = MutableFloat.fromValue((float) (Math.pow(eventDamage, 1.25)*Math.pow(eventDamage, 0.5)));
+						if (damage.getAsFloat() >= maxFloatValue){
+							damage = MutableFloat.fromValue(10000);
+						}
+						return EventResult.PASS;
+					}
+				}
+			}
+			return EventResult.PASS;
+		});
 		UseItemCallback.EVENT.register((player, world, hand) -> {
+			MinecraftServer server = world.getServer();
+			ServerPlayerEntity playerEntity = server.getPlayerManager().getPlayer(player.getUuid());
 			if (player.getInventory().getMainHandStack().isIn(ItemTags.SWORDS) && !player.getItemCooldownManager().isCoolingDown(player.getInventory().getMainHandStack().getItem())){
 				player.addExperience(1);
-				player.getItemCooldownManager().set(player.getInventory().getMainHandStack().getItem(), 10);
-				player.addStatusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 1, 0), player);
+				player.getItemCooldownManager().set(player.getInventory().getMainHandStack().getItem(), 20);
+				cooldown = 20;
 				//TODO add parrying
-
 				LOGGER.info("A sword");
 				return TypedActionResult.success(player.getActiveItem());
 			} else {
